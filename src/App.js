7faa7eb1586b762bc49ee56f7513d4c2f950707f
Lorrany Furtado <lorranyfdb@gmail.com>
2023-11-23 18:02:06 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+import PaginaInicial from './componentes/PaginaInicial';
+
+function App() {
+    return (
+        <div className="App">
+            <PaginaInicial />
+        </div>
+    );
+}
+export default App;
